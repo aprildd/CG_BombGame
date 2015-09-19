54514e6819ecceaef2ac0fbdb259db7f2052c6cb
aprildd <598165184@qq.com>
2015-09-20 01:57:28 +0800

    test scene

---
+++ trunk/project/CG_BombGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	private static LevelManager _instance = null;
+
+	public static LevelManager instance {
+		get {
+			if(!_instance) {
+
+				_instance = GameObject.FindObjectOfType(typeof(LevelManager)) as LevelManager;
+				if(!instance) {
+					GameObject container = new GameObject();
+					container.name = "LevelManager";
+					_instance = container.AddComponent<LevelManager>();
+				}
+			}
+
+			return _instance;
+		}
+	}
+}
